14dbc2d340c763b4b0c9f3ab320f22e642167065
murilo <muriloandradegandin@gmail.com>
2026-07-10 17:25:04 -0300

    revisão inicial

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+clientes = [
+    {
+        "id": 1,
+        "nome": "murilo",
+        "cidade": "Curitiba"
+    }
+]
+
+@app.get("/clientes")
+def listar_clientes():
+    return jsonify(clientes)
+
+@app.post("/clientes")
+def registrar_cliente():
+
+    dados = request.get_json()
+
+    novo = {
+        "id": len(clientes + 1),
+        "nome": dados["nome"],
+        "cidade": dados["cidade"]
+    }
+
+    clientes.append(novo)
+
+    return jsonify(novo), 201
+
+@app.put("/clientes/<int:id>")
+def atualizar_cliente(id):
+
+    dados = request.get_json()
+
+    for cliente in clientes:
+
+        if cliente["id"] == id:
+            cliente["nome"] == dados["id"]
+            cliente["cidade"] == dados["cidade"]
+
+            return jsonify(cliente)
+        
+    return {"erro": "Cliente não encontrado"}, 404
+
+@app.delete("/clientes/<int:id>")
+def remover_cliente(id):
+
+    for cliente in clientes:
+
+        if cliente["id"] == id:
+            clientes.remove(cliente)
+            return "", 204
+        
+    return {"erro": "Cliente não encontrado"}, 404
+
+if __name__ == "__main__":
+    app.run(debug=True)
