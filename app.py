398f6be2f8a7ef9224a7a5911b0b6e2f3a400dff
murilo <muriloandradegandin@gmail.com>
2026-07-11 21:05:13 -0300

    added databank and n8n requests

--- app.py
+++ app.py
@@ -1,72 +1,102 @@
 from flask import Flask, request, jsonify
+import sqlite3
 
 app = Flask(__name__)
 
-clientes = [
-    {
-        "id": 1,
-        "nome": "murilo",
-        "cidade": "Curitiba"
-    },
-    {
-        "id": 2,
-        "nome": "carlinhos",
-        "cidade": "SP"
-    }
-]
+conn = sqlite3.connect(
+    "clientes.db",
+    check_same_thread=False
+)
+conn.row_factory = sqlite3.Row
+cursor = conn.cursor()
+
 
 @app.get("/clientes")
 def listar_clientes():
-    return jsonify(clientes)
+
+    clientes = cursor.execute("""
+        SELECT * FROM clientes
+    """).fetchall()
+
+    return jsonify([dict(cliente) for cliente in clientes])
+
 
 @app.get("/clientes/<int:id>")
 def buscar_cliente(id):
-    for cliente in clientes:
-        if cliente["id"] == id:
-            return jsonify(cliente)
-        
-    return {"erro": "Cliente não encontrado"}, 404
+
+    cliente = cursor.execute("""
+        SELECT *
+        FROM clientes
+        WHERE id = ?
+    """, (id,)).fetchone()
+
+    if cliente is None:
+        return {"erro": "Cliente não encontrado"}, 404
+
+    return jsonify(dict(cliente))
+
 
 @app.post("/clientes")
 def registrar_cliente():
 
     dados = request.get_json()
 
-    novo = {
-        "id": len(clientes) + 1,
-        "nome": dados["nome"],
-        "cidade": dados["cidade"]
-    }
+    cursor.execute("""
+        INSERT INTO clientes (nome, cidade)
+        VALUES (?, ?)
+    """, (dados["nome"], dados["cidade"]))
 
-    clientes.append(novo)
+    conn.commit()
 
-    return jsonify(novo), 201
+    novo = cursor.execute("""
+        SELECT *
+        FROM clientes
+        WHERE id = last_insert_rowid()
+    """).fetchone()
+
+    return jsonify(dict(novo)), 201
+
 
 @app.put("/clientes/<int:id>")
 def atualizar_cliente(id):
 
     dados = request.get_json()
 
-    for cliente in clientes:
+    cursor.execute("""
+        UPDATE clientes
+        SET nome = ?, cidade = ?
+        WHERE id = ?
+    """, (dados["nome"], dados["cidade"], id))
 
-        if cliente["id"] == id:
-            cliente["nome"] == dados["id"]
-            cliente["cidade"] == dados["cidade"]
+    conn.commit()
 
-            return jsonify(cliente)
-        
-    return {"erro": "Cliente não encontrado"}, 404
+    if cursor.rowcount == 0:
+        return {"erro": "Cliente não encontrado"}, 404
+
+    cliente = cursor.execute("""
+        SELECT *
+        FROM clientes
+        WHERE id = ?
+    """, (id,)).fetchone()
+
+    return jsonify(dict(cliente))
+
 
 @app.delete("/clientes/<int:id>")
 def remover_cliente(id):
 
-    for cliente in clientes:
+    cursor.execute("""
+        DELETE FROM clientes
+        WHERE id = ?
+    """, (id,))
 
-        if cliente["id"] == id:
-            clientes.remove(cliente)
-            return "", 204
-        
-    return {"erro": "Cliente não encontrado"}, 404
+    conn.commit()
+
+    if cursor.rowcount == 0:
+        return {"erro": "Cliente não encontrado"}, 404
+
+    return "", 204
+
 
 if __name__ == "__main__":
     app.run(debug=True)
